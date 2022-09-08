156cc6f1ce48dfcd66dbf90cec456967dc37bdef
Yunge Yu <46149323+xcloudyunx@users.noreply.github.com>
2022-09-09 00:53:07 +1200

    can upload files

--- __init__.py
+++ __init__.py
@@ -1,11 +1,27 @@
-from flask import Flask
-from flask import render_template
+import os
+from flask import Flask, render_template, request, flash, redirect, send_from_directory
+from werkzeug.utils import secure_filename
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
 
-    @app.route("/")
+    app.config["UPLOAD_FOLDER"] = "./FileTransfer/uploads"
+    app.config["SECRET_KEY"] = os.urandom(12).hex()
+
+    @app.route("/", methods=["GET", "POST"])
     def index():
-        return render_template(("index.html"))
+        if request.method == "POST":
+            file = request.files["file"]
+            if file.filename == '':
+                flash("No selected file")
+            if file:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+        return render_template("index.html")
+    
+    @app.route('/uploads/<name>')
+    def download_file(name):
+        return send_from_directory(app.config["UPLOAD_FOLDER"], name)
+    
     
     return app
